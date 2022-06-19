48208336dddddd522ad365f4bc1d5f63a852a943
Color_yr <402067010@qq.com>
2022-06-19 13:51:58 +0800

    Update ImageWindow.xaml.cs

--- src/ColoryrBuild/Windows/ImageWindow.xaml.cs
+++ src/ColoryrBuild/Windows/ImageWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,19 +13,25 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace ColoryrBuild.Windows
-{
-    /// <summary>
-    /// ImageWindow.xaml 的交互逻辑
-    /// </summary>
-    public partial class ImageWindow : Window
-    {
-        public ImageWindow(byte[] data)
-        {
-            InitializeComponent();
+namespace ColoryrBuild.Windows;
 
-            Show();
-            Activate();
-        }
+/// <summary>
+/// ImageWindow.xaml 的交互逻辑
+/// </summary>
+public partial class ImageWindow : Window
+{
+    public ImageWindow(byte[] data)
+    {
+        InitializeComponent();
+        using MemoryStream stream = new(data);
+        var bitmapImage = new BitmapImage();
+        bitmapImage.BeginInit();
+        bitmapImage.StreamSource = stream;
+        bitmapImage.EndInit();
+        Img.Source = bitmapImage;
+        Width = bitmapImage.Width;
+        Height = bitmapImage.Height;
+        Show();
+        Activate();
     }
 }
